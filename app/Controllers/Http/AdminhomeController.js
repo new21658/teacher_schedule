d30ad516872752b9fef1b4a472e96b6ead73f059
new11439@gmail.com <new11439@gmail.com>
2018-04-24 23:11:18 +0700

    update dashboard

--- app/Controllers/Http/AdminhomeController.js
+++ app/Controllers/Http/AdminhomeController.js
@@ -1,9 +1,20 @@
 'use strict'
 
+const Course = use('App/Models/Course')
+const DB = use('Database')
+
 class AdminhomeController {
 
     async index({ view }) {
-        return view.render('admin.home');
+
+        let not_responsed = await DB.raw("select count(*) as not_responsed from courses as c where c.teacher_responsed = 0 and c.status = 'A'");
+        let not_approved = await DB.raw("select count(*) as not_approved from courses as c where c.approved = 0 and c.status = 'A'")
+
+        return view.render('admin.home', {
+            JSON: JSON,
+            not_responsed: not_responsed[0],
+            not_approved: not_approved[0]
+        });
     }
 
 }
